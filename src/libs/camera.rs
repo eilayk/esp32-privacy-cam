ac0b3bf59c52abe4e2825b8f5c634d1c4e334abf
Eilay K <22532056+eilayk@users.noreply.github.com>
2026-02-16 00:32:48 -0500

    feat: add camera support

---
+++ src/libs/camera.rs
@@ -0,0 +1,140 @@
+use std::marker::PhantomData;
+
+use esp_idf_svc::{
+    hal::gpio::{
+        Gpio10, Gpio11, Gpio12, Gpio13, Gpio15, Gpio16, Gpio17, Gpio18, Gpio4, Gpio5, Gpio6, Gpio7,
+        Gpio8, Gpio9, Pin,
+    },
+    sys::{
+        camera::{
+            camera_config_t, camera_config_t__bindgen_ty_1, camera_config_t__bindgen_ty_2,
+            camera_fb_location_t_CAMERA_FB_IN_PSRAM, camera_fb_t,
+            camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY, esp_camera_deinit, esp_camera_fb_get,
+            esp_camera_fb_return, esp_camera_init, framesize_t_FRAMESIZE_VGA,
+            ledc_channel_t_LEDC_CHANNEL_0, ledc_timer_t_LEDC_TIMER_0, pixformat_t_PIXFORMAT_JPEG,
+        },
+        ESP_OK,
+    },
+};
+
+// pin mappings defined here
+// https://docs.freenove.com/projects/fnk0083/en/latest/fnk0083/codes/C/Preface.html#cam-pin
+pub struct CameraPins {
+    pub siod: Gpio4,
+    pub sioc: Gpio5,
+    pub csi_vsync: Gpio6,
+    pub csi_href: Gpio7,
+    pub xclk: Gpio15,
+    pub csi_pclk: Gpio13,
+    pub csi_y9: Gpio16,
+    pub csi_y8: Gpio17,
+    pub csi_y7: Gpio18,
+    pub csi_y6: Gpio12,
+    pub csi_y5: Gpio10,
+    pub csi_y4: Gpio8,
+    pub csi_y3: Gpio9,
+    pub csi_y2: Gpio11,
+}
+
+pub struct Camera;
+
+pub struct Frame<'a> {
+    fb: *mut camera_fb_t,
+    // link Frame's lifetime to Camera's lifetime to ensure that Frame cannot outlive Camera
+    // prevent dropping Camera while Frame is still alive
+    _camera_phantom: core::marker::PhantomData<&'a Camera>,
+}
+
+impl<'a> Frame<'a> {
+    pub fn data(&self) -> &[u8] {
+        unsafe { core::slice::from_raw_parts((*self.fb).buf, (*self.fb).len) }
+    }
+
+    pub fn width(&self) -> usize {
+        unsafe { (*self.fb).width }
+    }
+
+    pub fn height(&self) -> usize {
+        unsafe { (*self.fb).height }
+    }
+}
+
+impl<'a> Drop for Frame<'a> {
+    fn drop(&mut self) {
+        unsafe {
+            esp_camera_fb_return(self.fb);
+        }
+    }
+}
+
+impl Camera {
+    pub fn init(pins: CameraPins) -> anyhow::Result<Self> {
+        // https://github.com/espressif/esp32-camera/tree/master?tab=readme-ov-file#initialization
+        let config = camera_config_t {
+            pin_pwdn: -1,
+            pin_reset: -1,
+            pin_xclk: pins.xclk.pin(),
+            __bindgen_anon_1: camera_config_t__bindgen_ty_1 {
+                pin_sccb_sda: pins.siod.pin(),
+            },
+            __bindgen_anon_2: camera_config_t__bindgen_ty_2 {
+                pin_sccb_scl: pins.sioc.pin(),
+            },
+
+            pin_d7: pins.csi_y9.pin(),
+            pin_d6: pins.csi_y8.pin(),
+            pin_d5: pins.csi_y7.pin(),
+            pin_d4: pins.csi_y6.pin(),
+            pin_d3: pins.csi_y5.pin(),
+            pin_d2: pins.csi_y4.pin(),
+            pin_d1: pins.csi_y3.pin(),
+            pin_d0: pins.csi_y2.pin(),
+
+            pin_vsync: pins.csi_vsync.pin(),
+            pin_href: pins.csi_href.pin(),
+            pin_pclk: pins.csi_pclk.pin(),
+
+            xclk_freq_hz: 20_000_000, // 20 MHz
+            ledc_timer: ledc_timer_t_LEDC_TIMER_0,
+            ledc_channel: ledc_channel_t_LEDC_CHANNEL_0,
+
+            pixel_format: pixformat_t_PIXFORMAT_JPEG,
+            frame_size: framesize_t_FRAMESIZE_VGA,
+
+            jpeg_quality: 10,
+            fb_count: 2,
+            fb_location: camera_fb_location_t_CAMERA_FB_IN_PSRAM,
+            grab_mode: camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY,
+            ..Default::default()
+        };
+
+        let ret = unsafe { esp_camera_init(&config) };
+        return match ret {
+            ESP_OK => Ok(Self),
+            _ => Err(anyhow::anyhow!(
+                "Failed to initialize camera: error code {}",
+                ret
+            )),
+        };
+    }
+
+    pub fn capture(&self) -> anyhow::Result<Frame> {
+        let fb = unsafe { esp_camera_fb_get() };
+        return if fb.is_null() {
+            Err(anyhow::anyhow!("Failed to capture frame"))
+        } else {
+            Ok(Frame {
+                fb,
+                _camera_phantom: PhantomData,
+            })
+        };
+    }
+}
+
+impl Drop for Camera {
+    fn drop(&mut self) {
+        unsafe {
+            esp_camera_deinit();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_svc::{hal::prelude::Peripherals, log::EspLogger, sys::link_patches};
+use std::result::Result::Ok;
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+use crate::libs::camera::{Camera, CameraPins};
 
-    log::info!("Hello, world!");
+mod libs;
+
+fn main() -> anyhow::Result<()> {
+    link_patches();
+    EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take()?;
+
+    log::info!("Initializing camera...");
+    let camera_pins = CameraPins {
+        siod: peripherals.pins.gpio4,
+        sioc: peripherals.pins.gpio5,
+        csi_vsync: peripherals.pins.gpio6,
+        csi_href: peripherals.pins.gpio7,
+        xclk: peripherals.pins.gpio15,
+        csi_pclk: peripherals.pins.gpio13,
+        csi_y9: peripherals.pins.gpio16,
+        csi_y8: peripherals.pins.gpio17,
+        csi_y7: peripherals.pins.gpio18,
+        csi_y6: peripherals.pins.gpio12,
+        csi_y5: peripherals.pins.gpio10,
+        csi_y4: peripherals.pins.gpio8,
+        csi_y3: peripherals.pins.gpio9,
+        csi_y2: peripherals.pins.gpio11,
+    };
+
+    let camera = Camera::init(camera_pins)?;
+    log::info!("Camera initialized successfully!");
+
+    log::info!("Starting frame capture loop...");
+    loop {
+        match camera.capture() {
+            Ok(frame) => {
+                let data = frame.data();
+                let width = frame.width();
+                let height = frame.height();
+                log::info!(
+                    "Captured frame: {}x{}, data size: {} bytes",
+                    width,
+                    height,
+                    data.len()
+                );
+            }
+            Err(e) => {
+                log::error!("Failed to capture frame: {}", e);
+            }
+        }
+    }
 }

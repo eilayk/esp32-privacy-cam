2f63b6bf142f0647f64e16925242e2723b33af5c
Eilay K <22532056+eilayk@users.noreply.github.com>
2026-03-11 19:23:56 -0400

    add esp-tflite-micro

--- src/libs/bindings.h
+++ src/libs/bindings.h
@@ -1,3 +1,10 @@
 #if defined(ESP_IDF_COMP_ESPRESSIF__ESP32_CAMERA_ENABLED)
 #include "esp_camera.h"
 #endif
+
+#if defined(ESP_IDF_COMP_ESPRESSIF__ESP_TFLITE_MICRO_ENABLED)
+#include "tensorflow/lite/c/c_api_types.h"
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/c/builtin_op_data.h"
+#include "esp_nn.h"
+#endif
